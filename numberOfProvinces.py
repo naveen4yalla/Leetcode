a2b686c81c571f9b6ffe448345124d6e38015ce8
Naveen Yalla <naveenyalla@Naveens-MacBook-Air.local>
2021-10-15 15:32:14 -0700

    numberOfProvinces1/usr/bin/python3

--- numberOfProvinces.py
+++ numberOfProvinces.py
@@ -1,3 +1,4 @@
+
 class Solution:
     def findCircle(self,isConnected):
         def dfs(f):
@@ -17,9 +18,50 @@ class Solution:
 
 
 S=Solution()
-print(S.findCircle([[1,0,0],[0,1,0],[0,0,1]]))
-
-#
+print(S.findCircle([[1,1,0,0],[1,2,0,1],[0,0,1,0],[0,1,0,1]]))
 #Space complexity is O(n)  
 #Time complexity is O(n²) 
+
+
+#Union-find Algorithm
+class UnionFind:
+    def __init__(self,n):
+        self.rank=[1]*n
+        self.root=[i for i in range(n)]
+        self.count=0
+    def find(self,a):
+        if a == self.root[a]:
+            return a
+        self.root[a] = self.find(self.root[a])
+        return self.root[a]
+
+    def unionFind(self,a,b):
+        xa=self.find(a)
+        xb=self.find(b)
+        if xa!=xb:
+            if self.rank[xa]>self.rank[xb]:
+                self.root[xb]=xa
+            elif self.rank[xa]<self.rank[xb]:
+                self.root[xa]=xb
+            else:
+                self.root[xb]=xa
+                self.rank[xa]=self.rank[xa]+1
+        self.count=self.count+1
+ 
+    def province(self):
+        return self.count
+
+class Solution1:
+    def findCircle(self,isConnected):
+        n=len(isConnected)
+        u=UnionFind(n)
+        for f in range(n):
+            for i in range(f+1,n):
+                if isConnected[f][i]==1:
+                    u.unionFind(f,i)
+        u.province()
+
+
+
+
 
